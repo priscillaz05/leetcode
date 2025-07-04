c72d6f200b3114b1a940e0d7a59d64d99028c644
Priscilla Zhu <159564715+priscillaz05@users.noreply.github.com>
2025-07-04 11:41:17 +0800

    File updated at 7/4/2025, 11:41:17 AM

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,4 +1,4 @@
-// Last updated: 7/4/2025, 11:40:04 AM
+// Last updated: 7/4/2025, 11:41:17 AM
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int[] ans = new int[m + n];
@@ -9,24 +9,18 @@ class Solution {
 
         while (i < m && j < n) {
             if (nums2[j] <= nums1[i]) {
-                ans[ind] = nums2[j];
-                ind++;
-                j++;
+                ans[ind++] = nums2[j++];
             } else if (nums1[i] < nums2[j]) {
-                ans[ind] = nums1[i];
-                ind++;
-                i++;
+                ans[ind++] = nums1[i++];
             }
         }
 
         for (; j < n; j++) {
-            ans[ind] = nums2[j];
-            ind++;
+            ans[ind++] = nums2[j];
         }
 
         for (; i < m; i++) {
-            ans[ind] = nums1[i];
-            ind++;
+            ans[ind++] = nums1[i];
         }
 
         int redo = 0;
