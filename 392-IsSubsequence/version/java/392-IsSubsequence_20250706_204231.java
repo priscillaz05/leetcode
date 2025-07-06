969d4cff26eb228c7774c0a57221bd6b5485d9cc
Priscilla Zhu <159564715+priscillaz05@users.noreply.github.com>
2025-07-06 20:42:33 +0800

    Create 392-IsSubsequence

---
+++ 392-IsSubsequence/version/java/392-IsSubsequence_20250706_204231.java
@@ -0,0 +1,24 @@
+// Last updated: 7/6/2025, 8:42:31 PM
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+                int sIdx = 0;
+        int tIdx = 0;
+        if (s.isEmpty()) {
+            return true;
+        }
+        if (t.isEmpty()) {
+            return false;
+        }
+        while (tIdx < t.length()) {
+            if (s.charAt(sIdx) == t.charAt(tIdx)) {
+                sIdx++;
+                if (sIdx >= s.length()) {
+                    return true;
+                }
+            }
+            tIdx++;
+        }
+
+        return false;
+    }
+}
