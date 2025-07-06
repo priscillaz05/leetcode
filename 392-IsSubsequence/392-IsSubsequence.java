c596709e0716670abba551f70ee7e82edeec2e32
Priscilla Zhu <159564715+priscillaz05@users.noreply.github.com>
2025-07-06 20:33:14 +0800

    File updated at 7/6/2025, 8:33:13 PM

--- 392-IsSubsequence/392-IsSubsequence.java
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -1,31 +1,24 @@
-// Last updated: 7/6/2025, 8:29:37 PM
+// Last updated: 7/6/2025, 8:33:13 PM
 class Solution {
     public boolean isSubsequence(String s, String t) {
-//        int[] visited = new int[s.length()];
-
-        for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
-//            System.out.println("cur: " + cur);
-
-            boolean found = false;
-            for (int j = 0; j < t.length(); j++) {
-//                System.out.println(t.length() + " " + j);
-                if (t.charAt(j) == cur) {
-//                    System.out.println("character in t:" + t.charAt(j));
-//                    if (i > 0 && j < visited[i - 1]) return false;
-
-                    found = true;
-                    t = t.substring(j + 1);
-//                    System.out.println("t: " + t);
-//                    visited[i] = j;
-                    break;
+                int sIdx = 0;
+        int tIdx = 0;
+        if (s.isEmpty()) {
+            return true;
+        }
+        if (t.isEmpty()) {
+            return false;
+        }
+        while (tIdx < t.length()) {
+            if (s.charAt(sIdx) == t.charAt(tIdx)) {
+                sIdx++;
+                if (sIdx >= s.length()) {
+                    return true;
                 }
             }
-
-            if (!found) return false;
+            tIdx++;
         }
 
-        return true;
-
+        return false;
     }
 }
