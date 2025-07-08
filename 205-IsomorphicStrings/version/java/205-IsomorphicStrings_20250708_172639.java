70894feb01b3811cf46153d28f97033f9a428b97
Priscilla Zhu <159564715+priscillaz05@users.noreply.github.com>
2025-07-08 17:26:40 +0800

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/version/java/205-IsomorphicStrings_20250708_172639.java
@@ -0,0 +1,22 @@
+// Last updated: 7/8/2025, 5:26:39 PM
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        // keep track of last appearing position of certain character
+        int[] sPos = new int[256];
+        int[] tPos = new int[256];
+
+        if (s.length() != t.length()) return false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            if (sPos[sChar] != tPos[tChar]) return false;
+
+            sPos[sChar] = i + 1;
+            tPos[tChar] = i + 1;
+        }
+
+        return true;
+    }
+}
