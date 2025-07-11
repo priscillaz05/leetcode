79266e23f02c5c371a1ae75b620714d86a6ac511
Priscilla Zhu <159564715+priscillaz05@users.noreply.github.com>
2025-07-11 21:40:40 +0800

    File updated at 7/11/2025, 9:40:39 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,26 +1,21 @@
-// Last updated: 7/11/2025, 9:31:28 PM
+// Last updated: 7/11/2025, 9:40:39 PM
 class Solution {
     public String sortString(String s) {
-        char[] c = s.toCharArray();
-        Arrays.sort(c);
-
-        return new String(c);
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<String> sorted = new ArrayList<>();
         HashMap<String, List<String>> dict = new HashMap<>();
 
         for (String str : strs) {
             String sortedStr = sortString(str);
 
             if (dict.containsKey(sortedStr)) {
-                List<String> tmp = dict.get(sortedStr);
-                tmp.add(str);
+                dict.get(sortedStr).add(str);
             } else {
-                List<String> tmp = new ArrayList<>();
-                tmp.add(str);
-                dict.put(sortedStr, tmp);
+                dict.put(sortedStr, new ArrayList<>(List.of(str)));
             }
         }
 
