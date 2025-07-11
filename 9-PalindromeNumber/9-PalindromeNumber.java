8a3e460738d533a0e1aa226c97a791f22a41df32
Priscilla Zhu <159564715+priscillaz05@users.noreply.github.com>
2025-07-11 22:15:07 +0800

    File updated at 7/11/2025, 10:15:06 PM

--- 9-PalindromeNumber/9-PalindromeNumber.java
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -1,24 +1,17 @@
-// Last updated: 7/11/2025, 10:12:08 PM
+// Last updated: 7/11/2025, 10:15:06 PM
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
 
-        int n = (int) Math.log10(x);
-//        System.out.println(n);
+        int reverse = 0;
+        int tmp = x;
 
-        int i = 1;
-        int j = n;
-
-        while (i <= j) {
-            int right = (int) (x % (Math.pow(10, i)) / (Math.pow(10, i - 1)));
-            int left = (int) (x / Math.pow(10, j) % 10);
-//            System.out.println("Right: " + right + " Left: " + left);
-            if (right != left) return false;
-
-            i++;
-            j--;
+        while (tmp != 0) {
+            int digit = tmp%10;
+            reverse = reverse*10 + digit;
+            tmp /= 10;
         }
 
-        return true;
+        return reverse==x;
     }
 }
