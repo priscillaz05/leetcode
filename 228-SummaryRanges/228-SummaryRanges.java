c157de16d3106b47ddf22234f148ca62bb089d78
Priscilla Zhu <159564715+priscillaz05@users.noreply.github.com>
2025-07-12 14:13:53 +0800

    File updated at 7/12/2025, 2:13:52 PM

--- 228-SummaryRanges/228-SummaryRanges.java
+++ 228-SummaryRanges/228-SummaryRanges.java
@@ -1,4 +1,4 @@
-// Last updated: 7/12/2025, 2:09:17 PM
+// Last updated: 7/12/2025, 2:13:52 PM
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> ans = new ArrayList<>();
@@ -6,17 +6,19 @@ class Solution {
         int i = 0;
         while (i < nums.length) {
             int st = nums[i];
+            StringBuilder str = new StringBuilder(st + "");
 
             int j = i + 1;
             for (; j <= nums.length; j++) {
                 if (j == nums.length || nums[j] != nums[j - 1] + 1) {
                     int end = nums[j - 1];
 
-                    if (st == end) {
-                        ans.add(st + "");
-                    } else {
-                        ans.add(st + "->" + end);
+                    if (st != end) {
+                        str.append("->");
+                        str.append(end);
                     }
+
+                    ans.add(str.toString());
                     break;
                 }
             }
