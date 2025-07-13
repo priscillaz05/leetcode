2416f8d053086d4dc835a7b8640c38aa067ef3b4
Priscilla Zhu <159564715+priscillaz05@users.noreply.github.com>
2025-07-13 14:04:04 +0800

    Create 28-FindtheIndexoftheFirstOccurrenceinaString

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -0,0 +1,21 @@
+// Last updated: 7/13/2025, 2:04:04 PM
+class Solution {
+    public int strStr(String haystack, String needle) {
+        char start = needle.charAt(0);
+        int n = needle.length();
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == start) {
+//                System.out.println(i + ": " + haystack.charAt(i));
+//                System.out.println(haystack.substring(i, i + n));
+
+
+                if (i + n <= haystack.length() && haystack.substring(i, i+n).equals(needle)) {
+                    return i;
+                }
+
+            }
+        }
+        return -1;
+    }
+}
